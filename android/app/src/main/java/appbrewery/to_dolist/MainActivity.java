f2de312bdf9ce025d7a3818074c8438eb7feed38
Elmir Nushi <elmir.nushi12@gmail.com>
2020-12-01 14:17:38 +0100

    Initial commit

---
+++ android/app/src/main/java/appbrewery/to_dolist/MainActivity.java
@@ -0,0 +1,6 @@
+package appbrewery.to_dolist;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(toDoList());
+
+class toDoList extends StatefulWidget {
+  @override
+  _toDoListState createState() => _toDoListState();
+}
+
+class _toDoListState extends State<toDoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
